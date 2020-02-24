0be2867d6b57a1c404eeb932159abbe08422e801
nicolasraffray <nraffray@gmail.com>
2020-02-24 15:52:28 +0000

    it now displays the date of the user action on the sheet

--- jasmine-3.5.0/spec/bankSpec.js
+++ jasmine-3.5.0/spec/bankSpec.js
@@ -5,6 +5,10 @@ describe('Bank', function(){
 
   beforeEach(function(){
     bank = new Bank 
+    var date = new Date(2020,0,1)
+      spyOn(window, 'Date').and.callFake(function(){
+        return date
+      })
   })
 
   describe('print_bank_balance', function(){
@@ -37,13 +41,19 @@ describe('Bank', function(){
   describe('update', function(){
     it('adds the deposit action to a new line of the balance sheet', function(){
       bank.deposit(100)
-      expect(bank.print_statement()).toEqual("Date||Credit||Debit||Balance\n|| || ||100||100")
+      expect(bank.print_statement()).toEqual("Date||Credit||Debit||Balance\n||01/01/2020|| ||100||100")
     })
 
     it('reduces the balance by the credit amount adding it to a new balance sheet line', function(){
       bank.balance = 100
       bank.withdraw(100)
-      expect(bank.print_statement()).toEqual("Date||Credit||Debit||Balance\n|| ||100|| ||0")
+      expect(bank.print_statement()).toEqual("Date||Credit||Debit||Balance\n||01/01/2020||100|| ||0")
+    })
+  })
+
+  describe('todays_date', function(){ 
+    it('generates the current date in the correct format for the user', function(){
+      expect(bank.todays_date()).toEqual('01/01/2020')
     })
   })
 })
--- jasmine-3.5.0/src/bank.js
+++ jasmine-3.5.0/src/bank.js
@@ -3,15 +3,28 @@ function Bank(){
   this.sheet = "Date||Credit||Debit||Balance"
   this.balance = 0
   this.update = function(amount,balance,debit){ 
-    if(debit === true){ this.sheet = `${this.sheet}\n|| || ||${amount}||${balance}`}
-    else { this.sheet = `${this.sheet}\n|| ||${amount}|| ||${balance}`} }
+    if(debit === true){ this.sheet = `${this.sheet}\n||${this.todays_date()}|| ||${amount}||${balance}`}
+    else { this.sheet = `${this.sheet}\n||${this.todays_date()}||${amount}|| ||${balance}`} }
 }
 
 
 Bank.prototype.print_statement = function(){ return this.sheet }
+
 Bank.prototype.deposit = function(amount){ 
   this.balance += amount
-  this.update( amount, this.balance, true ) }
+  this.update( amount, this.balance, true ) 
+}
+
 Bank.prototype.withdraw = function(amount){ 
   this.balance -= amount 
-  this.update( amount, this.balance, false )}
+  this.update( amount, this.balance, false )
+}
+
+Bank.prototype.todays_date = function(){
+  var today = new Date()
+  var dd = String(today.getDate()).padStart(2, '0')
+  var mm = String(today.getMonth() + 1).padStart(2, '0')
+  var yyyy = today.getFullYear();
+
+  return today = dd + '/' + mm + '/' + yyyy
+}
